3cad8dacab702d21e6a5acf5b3bfbd45c1879ae7
Karim Al-Sherif <stronciy@gmail.com>
2025-12-09 13:06:21 +0200

    Fix blaze route

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     return [
       { source: '/wp-json/wp/v2/posts', destination: '/api/wp/v2/posts' },
       { source: '/wp-json/blaze/v1/:path*', destination: '/api/blaze/v1/:path*' },
+      { source: '/blaze/v1/:path*', destination: '/api/blaze/v1/:path*' },
     ]
   },
 }
